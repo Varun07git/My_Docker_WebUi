6b86e4edd7f445d738fe3cc9d9fd5a1bcf61e4bf
Varun07git <68010309+Varun07git@users.noreply.github.com>
2022-09-09 19:44:50 +0530

    Now only docker cmd will run using cmd line ui

--- cgi-bin/web.py
+++ cgi-bin/web.py
@@ -10,9 +10,10 @@ mydata = cgi.FieldStorage()
 myx = mydata.getvalue("x")
 
 #myy = mydata.getvalue("y")
-
-
-output = subprocess.getoutput("sudo " + myx)
-print(output)
+if (myx[1] == "d" and myx[2]=="o" and myx[3]=="c" and myx[4]=="k" and myx[5]=="e" and myx[6]=="r"):
+    output = subprocess.getoutput("sudo " + myx)
+    print(output)
+else:
+    print("Other than docker command is not allowed to execute")
 #outputy = subprocess.getoutput("sudo " + imyy)
 #print(outputy)
